431bee8848991228e60685f39970edea88b7dc8d
KPSecretAccount <sampleemail4github@gmail.com>
2025-12-04 10:23:21 -0500

    ;)

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,6 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    "/: city/: state/: lat/: lon" element {<weather/>}
   </StrictMode>,
 )
