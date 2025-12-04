7e5abd3348b0854ef0cf8e77c7f571aa09b6a46a
KPSecretAccount <sampleemail4github@gmail.com>
2025-12-04 10:35:51 -0500

    ;)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect, useState} from 'react'
+//import type {City, CitySearchResult, Location, Weather} from "./main.tsx";
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const [currsearch, setcurrsearch] = useState<String>("Freehold");
+const autocomplete = useAutocomplete(currsearch);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+(input value = {currsearch}
+onchange = {e => setcurrsearch(e.target.value)}
+placeholder = "Search...")
+fetch('https://api.geoapify.com/v1/geocode/search?text=marlboro,%20nj&lang=en&limit=1&type=city&filter=countrycode:us&format=json&apiKey=b8568cb9afc64fad861a69edbddb2658')
+fetch('https://api.geoapify.com/v1/geocode/search?text=${marlboro%20nj}&lang=en&limit=1&type=city&filter=countrycode:us&format=json&apiKey=b8568cb9afc64fad861a69edbddb2658')
+fetch('https://api.geoapify.com/v1/geocode/autocomplete?text=${marlboro,%20nj}&type=city&limit=10&filter=countrycode%3Aus&format=json&apiKey=b8568cb9afc64fad861a69edbddb2658,')
+
+
+export default function UseWeather() {
+    const [weather, setWeather] = useState<Weather>([]);
+    const [error, setError] = useState<String | null>(null);
+    useEffect((): void => {
+        fetch('https://api.geoapify.com/v1/geocode/search?text=marlboro&lang=en&limit=1&type=city&filter=countrycode:us&format=json&apiKey=b8568cb9afc64fad861a69edbddb2658,')
+            .then(response => response.json())
+            .then(data => console.log(data)).catch(err => console.log(err));
+    })
 }
+
+export default function useLocation (SearchedValue: String|null) : [weather|null, string|null]{
+}
+
+export default function autocomplete ()
 
 export default App
