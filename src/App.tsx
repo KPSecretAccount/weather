2c6757d220e031689cb03a454ea32c09b2fcc6db
KPSecretAccount <sampleemail4github@gmail.com>
2025-12-19 12:22:44 -0500

    ;)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,14 @@ import { useState } from 'react';
 import './App.css';
 import { useAutocomplete, useLocation, useWeather } from "./hook";
 
+new Date("t").toLocaleString("en-US")
+{
+    year: "numeric";
+    month: "numeric";
+    day: "numeric";
+    hour: "numeric";
+    minute: "numeric";
+}
 export default function App() {
     const [currSearch, setCurrSearch] = useState<string>("");
     const [currPlace, setCurrPlace] = useState<string>("");
@@ -17,18 +25,6 @@ export default function App() {
                 <input type="text" name="text" className="input" value={currSearch}
                        onChange={e => setCurrSearch((e.target as HTMLInputElement).value)}
                        placeholder="Type here to search"/>
-                <button onClick={() => console.log("Search clicked")}>Search<span className="icon">
-    <svg width="19px" height="19px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g
-        id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round"
-                                                       stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path
-        opacity="1" d="M14 5H20" stroke="#000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path
-        opacity="1" d="M14 8H17" stroke="#000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path
-        d="M21 11.5C21 16.75 16.75 21 11.5 21C6.25 21 2 16.75 2 11.5C2 6.25 6.25 2 11.5 2" stroke="#000"
-        stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"></path> <path opacity="1" d="M22 22L20 20"
-                                                                                        stroke="#000" stroke-width="3.5"
-                                                                                        stroke-linecap="round"
-                                                                                        stroke-linejoin="round"></path> </g></svg>
-  </span></button>
             </div>
 
             <div>
